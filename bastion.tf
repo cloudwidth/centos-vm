3b067bd4b1def049231514aa307d60ca306d7f14
David Smith <dsmith@10thmagnitude.com>
2021-01-05 14:30:02 -0600

    added AzureBastionSubnet

--- bastion.tf
+++ bastion.tf
@@ -6,6 +6,13 @@ resource "azurerm_public_ip" "bastion-pip" {
   sku                 = "Standard"
 }
 
+resource "azurerm_subnet" "bastionsubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 resource "azurerm_bastion_host" "bastion-host" {
   name                = "bastion"
   location            = azurerm_resource_group.rg.location
@@ -13,7 +20,7 @@ resource "azurerm_bastion_host" "bastion-host" {
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.subnet1.id
+    subnet_id            = azurerm_subnet.bastionsubnet.id
     public_ip_address_id = azurerm_public_ip.bastion-pip.id
   }
 }
